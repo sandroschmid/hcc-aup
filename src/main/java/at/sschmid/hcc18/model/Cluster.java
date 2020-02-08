4fee941420781470e13128972cc1566774c3d940
Sandro Schmid <sandro.schmid@dynatrace.com>
2020-02-08 19:50:22 +0100

    Cleanup

--- src/main/java/at/sschmid/hcc18/model/Cluster.java
+++ src/main/java/at/sschmid/hcc18/model/Cluster.java
@@ -1,5 +1,6 @@
 package at.sschmid.hcc18.model;
 
+import lombok.Getter;
 import weka.clusterers.SimpleKMeans;
 import weka.core.Attribute;
 import weka.core.EuclideanDistance;
@@ -11,25 +12,26 @@ import java.util.Enumeration;
 import java.util.Locale;
 
 public class Cluster {
-  
+
   private static final NumberFormat DECIMAL_FORMAT;
   private static final int MAX_ATTR_PER_PAGE = 22;
-  
+
   static {
     DECIMAL_FORMAT = NumberFormat.getNumberInstance(Locale.US);
     DECIMAL_FORMAT.setMaximumFractionDigits(6);
   }
-  
+
+  @Getter
   private final int k;
   private final SimpleKMeans kMeans;
-  
+
   private Instances data;
-  
+
   public Cluster(final int k, final int maxIterations) throws Exception {
     this.k = k;
     this.kMeans = createKMeans(k, maxIterations);
   }
-  
+
   private SimpleKMeans createKMeans(final int k, final int maxIterations) throws Exception {
     final SimpleKMeans kMeans = new SimpleKMeans();
     kMeans.setNumClusters(k);
@@ -38,23 +40,19 @@ public class Cluster {
     kMeans.setPreserveInstancesOrder(true);
     return kMeans;
   }
-  
-  public int k() {
-    return k;
-  }
-  
+
   public void cluster(final Instances data) throws Exception {
     this.data = data;
-    
+
     kMeans.setDistanceFunction(new EuclideanDistance(data));
     kMeans.buildClusterer(data);
   }
-  
+
   @Override
   public String toString() {
     return kMeans.toString();
   }
-  
+
   public String asLatexTable() {
     final StringBuilder tableStart = new StringBuilder("\\begin{sidewaystable}")
         .append("\n  \\centering")
@@ -66,9 +64,9 @@ public class Cluster {
     for (int i = 1; i <= k; i++) {
       tableStart.append(" & Cluster ").append(i);
     }
-    
+
     tableStart.append(" \\\\\\hline");
-    
+
     final StringBuilder pages = new StringBuilder();
     final int numAttributes = data.numAttributes();
     final int cntPages = (int) Math.ceil(numAttributes / (double) MAX_ATTR_PER_PAGE);
@@ -76,15 +74,15 @@ public class Cluster {
       if (pageIdx > 0) {
         pages.append("\n");
       }
-      
+
       pages.append(tableStart)
           .append(latexTablePage(pageIdx * (MAX_ATTR_PER_PAGE + 1)))
           .append(latexTableEnd(pageIdx + 1));
     }
-    
+
     return pages.toString();
   }
-  
+
   private String latexTablePage(final int attrStart) {
     final StringBuilder page = new StringBuilder();
     final Instances centroids = kMeans.getClusterCentroids();
@@ -96,29 +94,29 @@ public class Cluster {
       attributeIt.nextElement();
       attrIdx++;
     }
-  
+
     while (attributeIt.hasMoreElements()) {
       final Attribute attribute = attributeIt.nextElement();
       final String attrName = attribute.name().replace("_", "\\_");
       page.append("\n    ").append(attrName).append(" & ");
-    
+
       for (int cIdx = 0; cIdx < k; cIdx++) {
         final Instance centroid = centroids.instance(cIdx);
         page.append(instanceToString(centroid, attribute)).append(" & ");
       }
-    
+
       page.replace(page.length() - " & ".length(), page.length(), " \\\\\\hline");
-    
+
       if (attrIdx == lastAttrIdx) {
         break;
       } else {
         attrIdx++;
       }
     }
-    
+
     return page.toString();
   }
-  
+
   private String latexTableEnd(final int page) {
     return new StringBuilder()
         .append("\n  \\end{tabularx}")
@@ -127,31 +125,31 @@ public class Cluster {
         .append("\n\\end{sidewaystable}\n")
         .toString();
   }
-  
+
   public String asCsv() {
     final StringBuilder csv = new StringBuilder("Attribute");
     for (int i = 1; i <= k; i++) {
       csv.append(",Cluster").append(i);
     }
-  
+
     final Instances centroids = kMeans.getClusterCentroids();
     final Enumeration<Attribute> attributeIt = data.enumerateAttributes();
     while (attributeIt.hasMoreElements()) {
       final Attribute attribute = attributeIt.nextElement();
       final String attrName = attribute.name();
       csv.append("\n").append(attrName).append(",");
-      
+
       for (int cIdx = 0; cIdx < k; cIdx++) {
         final Instance centroid = centroids.instance(cIdx);
         csv.append(instanceToString(centroid, attribute)).append(",");
       }
-      
+
       csv.deleteCharAt(csv.length() - 1);
     }
-    
+
     return csv.toString();
   }
-  
+
   private String instanceToString(final Instance instance, final Attribute attribute) {
     if (instance.isMissing(attribute)) {
       return attribute.isNumeric() ? "NaN" : "-";
@@ -162,5 +160,5 @@ public class Cluster {
       return instance.stringValue(attribute);
     }
   }
-  
+
 }
